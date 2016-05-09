3f76a02378a9428563f58d7638794d884507f098
Ben Roa <benihanas1@gmail.com>
2016-05-09 14:23:10 -0700

    Add initial implementation for GET responses

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,14 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+var dispatcher = require('httpdispatcher');
+var util   = require('util');
+var url   = require('url');
+var fs = require('fs');
+var qs = require('querystring');
+var http = require('http');
+
+exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -27,32 +34,103 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
+
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
+  console.log('The url parse: ', url.parse(request.url, true).pathname);
 
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  var pathname = url.parse(request.url, true).pathname;
+  var requestMethod = request.method;
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  if(pathname === '/classes/messages'){
+    if(requestMethod === 'GET'){
+     // console.log('THE REQUEST: ',url.parse(request.url, true) );    
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+      var statusCode = 200;
+      headers['Content-Type'] = 'text/json';
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify({results: [response]}));
+    
+    } else if (requestMethod === 'POST'){
+      
+      var requestBody = '';
+      request.on('data', function(data){
+        //console.log('data: ', data);
+        requestBody += data;
+      });
+
+
+      request.on('end', function() {
+        var formData = qs.parse(requestBody);
+      //  console.log('formData: ', formdata);
+        response.writeHead(201, {'Content-Type': 'text/html'});
+        response.end(requestBody);
+      });
+    }
+
+  } else if(pathname === '/send'){
+    if (requestMethod === 'POST'){
+          
+      var requestBody = '';
+      request.on('data', function(data){
+        requestBody += data;
+      });
+
+      request.on('end', function() {
+        var formData = qs.parse(requestBody);
+        response.writeHead(201, {'Content-Type': 'text/html'});
+      //  response.write('<!doctype html><html><head><title>response</title></head><body>');
+      //  response.write('Thanks for the data!<br />User Name: '+formData.UserName);
+        response.end(requestBody);
+      });
+    }
+  } else if(pathname === '/classes/rooms'){
+    if (requestMethod === 'POST'){ 
+      var requestBody = '';
+      request.on('data', function(data){
+        requestBody += data;
+      });
+
+      request.on('end', function() {
+        var formData = qs.parse(requestBody);
+        response.writeHead(201, {'Content-Type': 'text/html'});
+    //  response.write('<!doctype html><html><head><title>response</title></head><body>');
+    //  response.write('Thanks for the data!<br />User Name: '+formData.UserName);
+    //  response.write('<br />Repository Name: '+formData.Repository);
+    //  response.write('<br />Branch: '+formData.Branch);
+        response.end('</body></html>');
+      });
+    }
+  }
+
+
+  // // The outgoing status.
+  // var statusCode = 200;
+
+  // // See the note below about CORS headers.
+  // var headers = defaultCorsHeaders;
+
+  // // Tell the client we are sending them plain text.
+  // //
+  // // You will need to change this if you are sending something
+  // // other than plain text, like JSON or HTML.
+  // headers['Content-Type'] = 'text/json';
+
+  // // .writeHead() writes to the request line and headers of the response,
+  // // which includes the status and all headers.
+  // response.writeHead(statusCode, headers);
+
+  // // Make sure to always call response.end() - Node may not send
+  // // anything back to the client until you do. The string you pass to
+  // // response.end() will be the body of the response - i.e. what shows
+  // // up in the browser.
+  // //
+  // // Calling .end "flushes" the response's internal buffer, forcing
+  // // node to actually send all the data over to the client.
+  // response.end(JSON.stringify({results: []}));
+  //response.end('Hello, World!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
